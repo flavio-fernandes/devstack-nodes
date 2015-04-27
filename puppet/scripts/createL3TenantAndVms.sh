d61b7e80d47da997e528454a6ab62134efd03be8
Flavio Fernandes <ffernand@redhat.com>
2015-04-27 06:42:11 -0400

    Augmentations for doing basic l3

---
+++ puppet/scripts/createL3TenantAndVms.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#
+
+TNT_ID=${TNT_ID:-1}
+SUBNETS_PER_TENANT=${SUBNETS_PER_TENANT:-2}
+VMS_PER_SUBNET=${VMS_PER_SUBNET:-2}
+
+cd /home/vagrant/devstack
+
+source openrc admin admin
+
+keystone tenant-create --name=l3tenant${TNT_ID} --enabled=true
+keystone user-create --name=l3user${TNT_ID} --pass=l3user${TNT_ID} --email=l3user${TNT_ID}@example.com
+keystone user-role-add --user=l3user${TNT_ID} --role=Member --tenant=l3tenant${TNT_ID}
+
+source openrc l3user${TNT_ID} l3tenant${TNT_ID} ; export OS_PASSWORD=l3user${TNT_ID}
+
+if [ ! -f id_rsa_demo.pub ]; then ssh-keygen -t rsa -b 2048 -N '' -f id_rsa_demo; fi
+nova keypair-add --pub-key  id_rsa_demo.pub  demo_key
+
+nova secgroup-create sec1 sec1
+nova secgroup-add-rule sec1 icmp -1 -1 0.0.0.0/0
+for x in tcp udp; do nova secgroup-add-rule sec1 ${x} 1 65535 0.0.0.0/0 ; done
+
+neutron router-create l3tenant${TNT_ID}router
+
+for netIndex in `seq 1 ${SUBNETS_PER_TENANT}` ; do \
+  neutron net-create net${netIndex}
+  neutron subnet-create --gateway=${netIndex}.0.0.254 --name=subnet${netIndex} net${netIndex} ${netIndex}.0.0.0/24 --enable-dhcp
+  neutron router-interface-add l3tenant${TNT_ID}router subnet${netIndex}
+  sleep 5
+done
+neutron router-port-list l3tenant${TNT_ID}router
+
+IMAGE=$(nova image-list | grep 'cirros.*uec\s' | awk '{print $2}')
+
+for netIndex in `seq 1 ${SUBNETS_PER_TENANT}` ; do \
+    NETID=$(neutron net-list | grep -w net${netIndex} | awk '{print $2}')
+    for vmIndex in `seq 1 ${VMS_PER_SUBNET}` ; do \
+        VMNAME="${TNT_ID}_net${netIndex}_vm${vmIndex}"
+        echo creating ${VMNAME}
+        nova boot --poll --flavor m1.nano --image ${IMAGE} --nic net-id=${NETID} \
+        --security-groups sec1 --key-name demo_key \
+        ${VMNAME}
+        sleep 5
+    done
+done
+
+#
+# source openrc user1 tenant1 ; export OS_PASSWORD=user1
+# source openrc user2 tenant2 ; export OS_PASSWORD=user2
+# source openrc user${TNT_ID} tenant${TNT_ID} ; export OS_PASSWORD=user${TNT_ID}
+# source openrc admin admin
+#
+
