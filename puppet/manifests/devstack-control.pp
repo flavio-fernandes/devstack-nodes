5695e69b807c68e48c2a35b77bfb394a3cd3bd13
Flavio Fernandes <ffernand@redhat.com>
2015-08-12 17:12:11 -0400

    Warm Maria up to avoid stack failure
    While using centos 7 vm from Vagrant for O/S, I always hit this issue [1] in the very first stack.sh call.
    
    This is a preemptive way of avoiding it all together: explicitly adding mariabd pkg before stacking and then
    setting the password to mysql. I’m actually curious to know how this is not an issue when using odl-openstack-ci.
    
    [1]:
    2015-08-11 22:47:37.014 | Requirement already satisfied (use --upgrade to upgrade): jsonpatch<2,>=0.10 in /usr/lib/python2.7/site-packages (from warlock<2,>=1.0.1->python-glanceclient<0.18.0,>=0.15.0->python-openstackclient<1.1.0,>=1.0.3)
    2015-08-11 22:47:37.014 | Requirement already satisfied (use --upgrade to upgrade): functools32 in /usr/lib/python2.7/site-packages (from jsonschema<3,>=0.7->warlock<2,>=1.0.1->python-glanceclient<0.18.0,>=0.15.0->python-openstackclient<1.1.0,>=1.0.3)
    2015-08-11 22:47:37.014 | Requirement already satisfied (use --upgrade to upgrade): jsonpointer>=1.9 in /usr/lib/python2.7/site-packages (from jsonpatch<2,>=0.10->warlock<2,>=1.0.1->python-glanceclient<0.18.0,>=0.15.0->python-openstackclient<1.1.0,>=1.0.3)
    2015-08-11 22:47:37.014 | Building wheels for collected packages: cliff-tablib
    2015-08-11 22:47:37.014 |   Running setup.py bdist_wheel for cliff-tablib
    2015-08-11 22:47:37.247 |   Stored in directory: /root/.cache/pip/wheels/cb/3d/ff/2cbd7ba84a7e23e485c8b0875d8ef76a7c02cb468f330d1e9e
    2015-08-11 22:47:37.263 | Successfully built cliff-tablib
    2015-08-11 22:47:37.263 | Installing collected packages: tablib, cliff-tablib, python-openstackclient
    2015-08-11 22:47:38.133 | Successfully installed cliff-tablib-1.1 python-openstackclient-1.0.4 tablib-0.10.0
    2015-08-11 22:47:38.183 | Redirecting to /bin/systemctl restart  rabbitmq-server.service
    2015-08-11 22:47:39.151 | Creating user "stackrabbit" ...
    2015-08-11 22:47:39.159 | ...done.
    2015-08-11 22:47:39.280 | Setting permissions for user "stackrabbit" in vhost "/" ...
    2015-08-11 22:47:39.289 | ...done.
    2015-08-11 22:47:39.397 | Changing password for user "stackrabbit" ...
    2015-08-11 22:47:39.405 | ...done.
    2015-08-11 22:47:39.429 | Redirecting to /bin/systemctl start  mariadb.service
    2015-08-11 22:47:41.881 | Redirecting to /bin/systemctl restart  mariadb.service
    2015-08-11 22:47:45.929 | LOGDIR: /opt/stack/logs
    2015-08-11 22:47:45.930 | SCREEN_LOGDIR: /opt/stack/data/log
    2015-08-11 22:47:45.930 | log: /opt/stack/logs/dstat.log.2015-08-11-223433
    2015-08-11 22:47:49.034 | ERROR 1045 (28000): Access denied for user 'root'@'devstack-control' (using password: YES)
    2015-08-11 22:47:49.035 | Error on exit

--- puppet/manifests/devstack-control.pp
+++ puppet/manifests/devstack-control.pp
@@ -1,9 +1,36 @@
+$deps = [
+    'mariadb-server',
+]
 
 $hosts = hiera('hosts')
 
 file { '/home/vagrant/devstack':
    ensure => 'link',
    target => '/opt/devstack',
+}
+
+package { $deps:
+    ensure   => installed,
+}
+
+# Warm Maria up, so initial password is non-empty
+# Ref: https://gist.github.com/333007187d69fe2fe282
+#
+service { 'mariadb.service':
+    ensure => 'running',
+    require => Package[$deps],
+}
+file { '/tmp/setup_mysql.txt':
+    ensure  => present,
+    owner   => 'vagrant',
+    group   => 'vagrant',
+    content => template('/vagrant/puppet/templates/setup_mysql.erb'),
+}
+exec { 'Warmup mysql':
+    command => '/usr/bin/mysql --user=root --password="" mysql < /tmp/setup_mysql.txt',
+    user    => 'vagrant',
+    onlyif  => ['/usr/bin/test -f /usr/bin/mysql'],
+    require => [File['/tmp/setup_mysql.txt'], Service['mariadb.service']],
 }
 
 vcsrepo { '/opt/devstack':
